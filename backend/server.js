a3d34f84f0444e5a02b34775dfdb049bb6fb27b0
marymo <mo.mary.gabin@gmail.com>
2025-03-12 13:33:34 -0600

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./src/config/db");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(require("cors")());
+app.use(require("helmet")());
+
+// Connect to Database
+if (process.env.MONGO_RUI) {
+  connectDB();
+} else {
+  console.log("⚠️ Skipping MongoDB connection (MONGO_URI not set)")
+}
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("🚀 StockEd Backend Running!");
+});
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
