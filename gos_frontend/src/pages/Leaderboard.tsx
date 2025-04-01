d771a00a611619518429fd3701791801a4a65f03
jennydang61 <jennydang61@gmail.com>
2025-03-31 23:25:45 -0600

    add headers

--- gos_frontend/src/pages/Leaderboard.tsx
+++ gos_frontend/src/pages/Leaderboard.tsx
@@ -6,7 +6,7 @@ const Leaderboard = () => {
       <div className="leaderboardPage">
         <NavBar />
         <main className="leaderboardContainer">
-
+        <h1 className="leaderboardTitle">Leaderboard</h1>
         </main>
       </div>
     );
