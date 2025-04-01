9249da6b5255224f1fc769f6d0e9bd4e35aaa621
jennydang61 <jennydang61@gmail.com>
2025-03-31 23:24:26 -0600

    create leaderboard, stock, and profile page

---
+++ gos_frontend/src/pages/Leaderboard.tsx
@@ -0,0 +1,15 @@
+import NavBar from "../components/ui/Navbar2";
+import '../../styles/Leaderboard.css';
+
+const Leaderboard = () => {
+    return (
+      <div className="leaderboardPage">
+        <NavBar />
+        <main className="leaderboardContainer">
+
+        </main>
+      </div>
+    );
+  };
+  
+  export default Leaderboard;
