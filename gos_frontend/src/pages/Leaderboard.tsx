c882773fd8b2594b601959c11f45cb522e0a3efc
jennydang61 <jennydang61@gmail.com>
2025-04-04 00:18:42 -0600

    add leaderboard

--- gos_frontend/src/pages/Leaderboard.tsx
+++ gos_frontend/src/pages/Leaderboard.tsx
@@ -2,14 +2,22 @@ import NavBar from "../components/ui/Navbar";
 import '../../styles/Leaderboard.css';
 
 const Leaderboard = () => {
-    return (
-      <div className="leaderboardPage">
-        <NavBar />
-        <main className="leaderboardContainer">
+  return (
+    <div className="leaderboardPage">
+      <NavBar />
+      <main className="leaderboardContainer">
         <h1 className="leaderboardTitle">Leaderboard</h1>
-        </main>
-      </div>
-    );
-  };
-  
-  export default Leaderboard;
+        <div className="leaderboardHeader">
+          <span>Ranking</span>
+          <span>Total Assets</span>
+          <span>Daily Return</span>
+        </div>
+        <div className="leaderboardList">
+          {/* User rows will go here later */}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Leaderboard;
