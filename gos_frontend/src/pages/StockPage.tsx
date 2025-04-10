ff21ffd57b9179a35e12b07bed10438995d8fd11
jennydang61 <jennydang61@gmail.com>
2025-04-09 23:38:48 -0600

    fix stock page

--- gos_frontend/src/pages/StockPage.tsx
+++ gos_frontend/src/pages/StockPage.tsx
@@ -3,19 +3,24 @@ import '../../styles/StockPage.css';
 import searchImage from "/src/images/searchIcon.png";
 
 const Stockpage = () => {
-    return (
-      <div className="stockPage">
-        <NavBar />
-        <main className="stockPageContainer">
-          <h1 className="stockPageTitle">Trade Stocks</h1>
-  
+  return (
+    <div className="stockPage">
+      <NavBar />
+      <main className="stockPageContainer">
+        <h1 className="stockPageTitle">Trade Stocks</h1>
+
+        <div className="searchBarWrapper">
           <div className="searchBar">
             <img src={searchImage} className="searchIcon" />
             <input type="text" placeholder="Search for stocks" />
           </div>
-        </main>
-      </div>
-    );
-  };
-  
-  export default Stockpage;
+        </div>
+        {/* stocks */}
+        <div className="stockGrid">
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Stockpage;
