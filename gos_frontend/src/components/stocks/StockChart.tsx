d772a3f9104e463eaf77809eba265e61eb41a8e4
marymo <mo.mary.gabin@gmail.com>
2025-04-21 14:03:12 -0600

    stock chart label updated

--- gos_frontend/src/components/stocks/StockChart.tsx
+++ gos_frontend/src/components/stocks/StockChart.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer,
 } from "recharts";
 
 interface StockChartProps {
@@ -12,18 +11,44 @@ const StockChart: React.FC<StockChartProps> = ({ data }) => {
   return (
     
     <ResponsiveContainer width="100%" height={400}>
-      
       <LineChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" tick={false} axisLine={false} />
-        <YAxis domain={['auto', 'auto']} tick={false} axisLine={false} />
-        <Tooltip />
+        <XAxis dataKey="date" tick={true} axisLine={false} />
+        <YAxis domain={['auto', 'auto']} tick={true} axisLine={false} />
+        <Tooltip
+          content={({ active, label, payload }) => {
+            if (active && payload && payload.length) {
+              const formattedDate = new Date(label).toLocaleDateString([], {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+              });
+              const formattedTime = new Date(label).toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+              });
+
+              return (
+                <div>
+                  <p style={{ margin: 0 }}>
+                    <strong>Date:</strong> {formattedDate} {formattedTime}
+                  </p>
+                  <p style={{ margin: 0 }}>
+                    <strong>Price:</strong> ${payload[0].value.toFixed(2)}
+                  </p>
+                </div>
+              );
+            }
+            return null;
+          }}
+        />
         <Line
           type="monotone"
           dataKey="close"
           stroke="#8884d8"
           strokeWidth={2}
           dot={false}
+          isAnimationActive={false}
         />
       </LineChart>
     </ResponsiveContainer>
